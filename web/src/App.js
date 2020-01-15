c2fbb4d8b5acb8f0ffc4c23619d6f375a9fa12b0
Isaque Igor da Silva <isaqueigorsilva@gmail.com>
2020-01-15 15:24:43 -0200

    Frontend: structure web

---
+++ web/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import './global.css';
+import './App.css';
+
+
+function App() {
+  return (
+    <div id="App">
+
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="">Github User</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+          <div class="input-block">
+            <label htmlFor="">Github User</label>
+            <input name="github_username" id="github_username" required />
+         </div>
+          <div class="input-block">
+              <label htmlFor="">Github User</label>
+              <input name="github_username" id="github_username" required />
+          </div>
+        </form>
+      </aside>
+
+      <main>
+
+
+      </main>
+      
+    </div>
+  );
+}
+
+export default App;
